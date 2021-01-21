2b7406bd4a38c6cce4cf731e5dcbcd96a82d1d9e
Jijo Pulikkottil <jijo.pulikkottil@zcocorporation.com>
2021-01-21 16:05:19 +0530

    24

---
+++ Notification/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  Notification
+//
+//  Created by Jijo Pulikkottil on 21/01/21.
+//  Copyright © 2021 MVoc. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
